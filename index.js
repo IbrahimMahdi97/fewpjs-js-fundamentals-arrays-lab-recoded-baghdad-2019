d463ec87cf11814a943ea97888834038310cfaf8
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2019-09-22 14:43:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Write your solution here!
 let pets = ['Milo', 'Otis', 'Garfieled'];
 const append = pets;
+const prepend = pets;
+const removeLast = pets;
+const removeFirst = pets;
+append.push('Odie');
